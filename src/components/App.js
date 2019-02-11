23b23dbab8873ad5dc0b2b3bdd77524977d8a21e
hanasari <hanasari@stunitas.com>
2019-02-11 17:03:00 +0900

    study

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>schedule management</div>
+        );
+    }
+}
+
+export default App;
